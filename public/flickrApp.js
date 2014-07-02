5dad9cc5e9a7ecf34230d704d1f9dcc63814c5c3
ggholson <deviowa@Gregs-Macbook-Air.local>
2014-07-02 15:51:46 -0500

    Image loading worksgit status

--- public/flickrApp.js
+++ public/flickrApp.js
@@ -1,16 +1,30 @@
+function loadImages(imageData) {
+    var img;
+
+    for (i = 0; i < imageData.photos.photo.length; i++) {
+        img = $("<img>");
+        img.attr("src", 'https://farm' + imageData.photos.photo[i].farm + '.staticflickr.com/' + imageData.photos.photo[i].server + '/' + imageData.photos.photo[i].id + '_' + imageData.photos.photo[i].secret + '.jpg');
+        $('body').append(img);
+    }
+}
+
+function searchFlickrImages(text, page) {
+    var searchResult = {};
+
+    $.get("http://localhost:3000/search/?text=" + text + "&page=" + page, function(data) {
+
+        data = data.slice(14, -1);
+        console.log(data);
+        searchResult = JSON.parse(data);
+        loadImages(searchResult);
+    })
+}
+
 $(document).ready(function() {
-	$("#btnSubmit").click(function(){
-		alert("button");
-	}); 
+    $("button").on('click', function() {
+        searchFlickrImages($('#inputtext').val(), 1)
+    });
 });
 
-var loadFlickrImages=function(text, page) {
-	$.get("localhost:3000/search/?text="+text + "&page=" + "1", function(data) {
-		console.log(data)
-	})
-
-
-
-}
 
 /*http://jquery.com/download/*/
